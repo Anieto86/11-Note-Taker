1351550a9f74345564535cb86fb6031b2e7e6333
Alvaro Nieto <60838509+Anieto86@users.noreply.github.com>
2020-05-10 19:46:37 +0930

    JSON to db.json

--- app.js
+++ app.js
@@ -11,21 +11,23 @@ let PORT = process.env.PORT || 3000;
 //they can modify your request before it gets to your handlers
 //it's like a pipeline, the request will move through those middlewares and they can optionally modify the request object
 
-// 
 app.use(express.static(__dirname + '/public/'));
 app.use(express.urlencoded({ extended: true })); // esto para decodificar el formaot URL 
 app.use(express.json()); // Esto lo convierte en JSON 
 
-//Data
-
 //GET method route
-app.get('/', function (req, res){
-  // res.sendFile(path.join(__dirname, '/db/db.json'))
-  //   res.send('welcome');
+app.get('/notes', function (req, res){
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
+
 app.get('/api/notes/', function(req, res){
-res.sendFile(path.join(__dirname, '/public/notes.html'))
+  filePath = __dirname + '/db/db.json'; 
+  fs.readFile(filePath, function(err, json) {
+    if (err) { throw err} 
+    res.send(JSON.parse(json))  // va a mandar el JSON parse que es el array en db.json  //se the JSON to db.jason
+  })
 });
+
 app.get('/index', function(req, res){
     res.json("index")
 });
@@ -51,7 +53,7 @@ app.post('/api/notes/', function (req, res) {
 app.delete('/api/notes/:id', function (req, res) {
     res.send('DELETE request to homepage')
     //req.query
-  })
+  });
 //Listener
 app.listen(PORT, function() {
   console.log('Server started on port ' + PORT);
